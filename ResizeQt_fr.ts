3744fbdcf277505e04ac3a78ae08954a34028dd4
Léo Baudouin <baudouin.leo@gmail.com>
2013-04-23 10:40:27 +0200

    Add menu, add icons, translate in french

---
+++ ResizeQt_fr.ts
@@ -0,0 +1,203 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>Resize</name>
+    <message>
+        <location filename="resize.ui" line="14"/>
+        <source>Resize</source>
+        <translation>Resize</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="25"/>
+        <source>Images</source>
+        <translation>Images</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="86"/>
+        <source>Options</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="97"/>
+        <source>Keep Exif info</source>
+        <translation>Garder les info Exif</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="107"/>
+        <source>Rotate if needed</source>
+        <translation>Tourner si besoin</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="117"/>
+        <source>Do not resize</source>
+        <translation>Ne pas redimensionner</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="146"/>
+        <source>Maximum size</source>
+        <translation>Taille maximum</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="193"/>
+        <source>pixels</source>
+        <translation>pixels</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="210"/>
+        <source>Ratio</source>
+        <translation>Ratio</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="251"/>
+        <source>%</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="271"/>
+        <source>Add logo</source>
+        <translation>Ajouter logo</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="283"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="296"/>
+        <location filename="resize.cpp" line="374"/>
+        <source>Logo</source>
+        <translation>Logo</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="308"/>
+        <source>Corner</source>
+        <translation>Coin</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="321"/>
+        <source>Top Left</source>
+        <translation>Haut gauche</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="337"/>
+        <source>Bottom Right</source>
+        <translation>Bas droite</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="347"/>
+        <source>Pos X</source>
+        <translation>Pos X</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="373"/>
+        <source>Pos Y</source>
+        <translation>Pos Y</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="438"/>
+        <source>Menu</source>
+        <translation>Menu</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="447"/>
+        <source>?</source>
+        <translation>?</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="457"/>
+        <source>Add files</source>
+        <translation>Ajouter des fichiers</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="460"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="468"/>
+        <source>Add folder</source>
+        <translation>Ajouter un dossier</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="471"/>
+        <source>Ctrl+Shift+O</source>
+        <translation>Ctrl+Shift+O</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="483"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="486"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="501"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="resize.ui" line="513"/>
+        <location filename="resize.cpp" line="537"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="98"/>
+        <source>Select a Folder</source>
+        <translation>Choisir un dossier</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="111"/>
+        <source>Select Files</source>
+        <translation>Choisir des ficihiers</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="111"/>
+        <source>Image files (*.jpg *.jpeg)</source>
+        <translation>Images (*.jpg *.jpeg)</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="139"/>
+        <source>Loading</source>
+        <translation>Chargement</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="139"/>
+        <location filename="resize.cpp" line="429"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="371"/>
+        <source>Select Logo</source>
+        <translation>Choisir un logo</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="371"/>
+        <source>Image files (*.jpg *.jpeg *.png)</source>
+        <translation>Image (*.jpg *.jpeg *.png)</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="429"/>
+        <source>Progress</source>
+        <translation>Progression</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="538"/>
+        <source>Written by %1 (%2)
+Version: %3</source>
+        <comment>author, year, version</comment>
+        <translation>Ecrit par %1 (%2)
+Version : %3</translation>
+    </message>
+    <message>
+        <location filename="resize.cpp" line="565"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+</context>
+</TS>
